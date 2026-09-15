d957343ddca2565d2605fba5a7a303cb214fcaf3
yoannmabit-design <yoannmabit@gmail.com>
2026-09-15 22:11:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    hors-ligne.
    ============================================================ */
 
-const VERSION = 'yfb-boutique-v23';
+const VERSION = 'yfb-boutique-vx';
 
 const ESSENTIELS = [
   './',
